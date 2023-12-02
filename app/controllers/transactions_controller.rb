ff89dde93814639a51c9aba82a81b16d92c7d476
SFarida <faridagueleu@gmail.com>
2023-12-02 15:34:06 +0100

    Update login and register form

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
   before_action :authenticate_user!, :set_category
 
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.order(created_at: :desc)
   end
 
   def show
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
     has_many :transactions, foreign_key: :category_id, dependent: :destroy
 
     validates :name, :icon, presence: true
+
+    def total_amount
+        transactions.sum(:amount)
+    end
 end
